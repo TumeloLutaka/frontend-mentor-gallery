df3088652c359601fda2b8ba4aa8f77b28c0a6be
Tumelo Lutaka <tlutaka@wiphanwz.local>
2026-04-17 13:58:33 +0200

    Working on dark and light mode switch, and readme modal.

--- index.js
+++ index.js
@@ -1,45 +1,111 @@
 /* =============================== */
 /* ========= DOM REFERENCE ======= */
 /* =============================== */
+// BUTTONS
+const readmeModalClose = document.getElementById("readme-modal__close");
+
+// DIALOGUE
+const readmeModal = document.getElementById("readme-modal");
+
 // DIVS
 const solutionsCardWrapper = document.getElementById(
   "solutions__cards-wrapper",
 );
 
-initialize();
+// INPUT
+const mainHeaderThemeToggle = document.getElementById(
+  "main-header__theme-checkbox",
+);
 
-async function initialize() {
+initialize();
+/* =============================== */
+/* =========== EVENT LISTENERS === */
+/* =============================== */
+mainHeaderThemeToggle.addEventListener("change", () => {
+  if (mainHeaderThemeToggle.checked) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+});
+
+readmeModalClose.addEventListener("click", () => {
+  readmeModal.close();
+});
+
+/* =============================== */
+/* =========== FUNCTIONS ========= */
+/* =============================== */
+function initialize() {
+  // SETTING THEME
+  // 1. Check if the user is on dark mode via system settings
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (prefersDark) {
+    document.body.classList.add("dark-mode");
+    mainHeaderThemeToggle.checked = true; // Make sure the UI switch matches!
+  } else {
+    document.body.classList.remove("dark-mode");
+    mainHeaderThemeToggle.checked = false;
+  }
+
+  loadCards();
+}
+
+async function loadCards() {
   try {
     const response = await fetch("./content.json");
     const solutions = await response.json();
 
-    console.log(solutions);
-    loadCards(solutions);
-  } catch (error) {
-    console.log(error);
-  }
-}
+    // Clear the children content from the card wrapper.
+    solutionsCardWrapper.innerHTML = "";
 
-function loadCards(solutions) {
-  // Clear the children content from the card wrapper.
-  solutionsCardWrapper.innerHTML = "";
-
-  // Loop through every solutions object and create an associated card element
-  solutions.forEach((sol) => {
-    solutionsCard = document.createElement("article");
-    solutionsCard.classList.add("solutions__card");
-    solutionsCard.innerHTML = `
+    // Loop through every solutions object and create an associated card element
+    solutions.forEach((sol) => {
+      solutionsCard = document.createElement("article");
+      solutionsCard.classList.add("solutions__card");
+      solutionsCard.innerHTML = `
         <div class="solutions__card-text-wrapper">
             <h3 class="solutions__card-heading heading">${sol.title}</h3>
             <p class="solutions__card-desc">${sol.description}</p>
         </div>
 
-        <div class="solutions__links">
-            <a class="solutions__link" href="${sol.live_url}">Live Site</a>
-            <a class="solutions__link" href="${sol.repo_url}">View Code</a>
+        <div class="solutions__card-actions">
+          <div class="solutions__links">
+            <a class="solutions__link button" href="${sol.live_url}">Live Site</a>
+            <a class="solutions__link button" href="${sol.repo_url}">View Code</a>
+            </div>
+          <button class="solutions_open-modal button button--ghost" onclick="openReadme('${sol.title}', '${sol.readme_url}')">Project Info</button>
         </div>
-    `;
+        `;
 
-    solutionsCardWrapper.appendChild(solutionsCard);
-  });
+      solutionsCardWrapper.appendChild(solutionsCard);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function openReadme(title, readmePath) {
+  readmeModal.showModal();
+  console.log(readmeModal);
+  const contentArea = readmeModal.querySelector("#readme-modal__content");
+
+  try {
+    // Fetch raw text from the project' README
+    const response = await fetch(readmePath);
+    const markdownText = await response.text();
+
+    // Change header title
+    readmeModalProjectTitle = document.getElementById(
+      "readme-modal__project-title",
+    );
+    readmeModalProjectTitle.textContent = title;
+
+    // Convert Markdown to HTML using the liabrary
+    contentArea.innerHTML = marked.parse(markdownText);
+    readmeModal.showModal();
+  } catch (error) {
+    contentArea.innerHTML = "<p>Doumentation could not be loaded.</p>";
+  }
 }
---
+++ solutions/frontend-quiz-app/README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Frontend quiz app solution
+
+This is a solution to the [Frontend quiz app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/frontend-quiz-app-BE7xkzXQnU). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Select a quiz subject
+- Select a single answer from each question from a choice of four
+- See an error message when trying to submit an answer without making a selection
+- See if they have made a correct or incorrect choice when they submit an answer
+- Move on to the next question after seeing the question result
+- See a completed state with the score after the final question
+- Play again to choose another subject
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- Navigate the entire app only using their keyboard
+- **Bonus**: Change the app's theme between light and dark
+
+### Screenshot
+
+![Desktop View Light Mode](/solutions/frontend-quiz-app/assets/images/screenshots/desktop-view-light.png)
+![Desktop View Dark Mode](/solutions/frontend-quiz-app/assets/images/screenshots/desktop-view-dark.png)
+
+### Built with
+
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@TumeloLutaka](https://www.frontendmentor.io/profile/TumeloLutaka)
+- Github - [@TumeloLutaka](https://github.com/TumeloLutaka)
